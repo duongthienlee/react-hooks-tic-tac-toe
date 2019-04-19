9f13ef3deedd9ab0427fd1a44adec0f19701c06d
duongthienlee <duongthienlee@gmail.com>
2019-04-20 00:34:09 +0300

    Add react-router-dom, gh-pages

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import Game from './Game';
 import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Game />, document.getElementById('root'));
+import { HashRouter, Route } from "react-router-dom"
+ReactDOM.render(<HashRouter><Route exact path="/" component={Game} /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
