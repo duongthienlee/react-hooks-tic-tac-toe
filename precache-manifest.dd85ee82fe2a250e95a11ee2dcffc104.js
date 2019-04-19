e954eaf49a9cb2e67d82f48d0be79d360b4f63f6
duongthienlee <duongthienlee@gmail.com>
2019-04-20 00:39:32 +0300

    Updates

---
+++ precache-manifest.dd85ee82fe2a250e95a11ee2dcffc104.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "683482640015b2255c696294593c86cf",
+    "url": "/react-hooks-tic-tac-toe/static/media/zToA.68348264.svg"
+  },
+  {
+    "revision": "e74e0075a73ad8c40c0eae3c1d893707",
+    "url": "/react-hooks-tic-tac-toe/static/media/aToZ.e74e0075.svg"
+  },
+  {
+    "revision": "8a647fa2f1613de30beb",
+    "url": "/react-hooks-tic-tac-toe/static/js/runtime~main.aa6fd2d1.js"
+  },
+  {
+    "revision": "1edff579936119c233bd",
+    "url": "/react-hooks-tic-tac-toe/static/js/main.b2ea5f55.chunk.js"
+  },
+  {
+    "revision": "9001770b91e4bcb77a23",
+    "url": "/react-hooks-tic-tac-toe/static/js/2.8da5b7c5.chunk.js"
+  },
+  {
+    "revision": "1edff579936119c233bd",
+    "url": "/react-hooks-tic-tac-toe/static/css/main.748f6fb3.chunk.css"
+  },
+  {
+    "revision": "9001770b91e4bcb77a23",
+    "url": "/react-hooks-tic-tac-toe/static/css/2.bea83a86.chunk.css"
+  },
+  {
+    "revision": "9bd59c76be38b6f0a88ccac4f192a9e5",
+    "url": "/react-hooks-tic-tac-toe/index.html"
+  }
+];
